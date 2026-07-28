c87f9ff960e08fe099772a9cbf9e70a44860e37a
Daniel Lima <137017200+DanielGLima@users.noreply.github.com>
2026-07-27 22:32:34 -0600

    feat: proyecto inicial conectado a Github

---
+++ src/test/java/com/sistemadequejas/SistemaDeQuejasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sistemadequejas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaDeQuejasApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
